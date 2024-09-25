9625ad7386e33c5aa3326261df3bca0718641544
diego <diegozaratean@gmail.com>
2024-09-25 23:43:24 +0000

    prep data modeling class

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('programación orientada a objetos')
+// Programación orientada a objetos es un modelo de programación
+
+// 1. Clases
+// 2. Objetos
+
+// 3. Herencia
+// 4. Encapsulamiento
+// 5. Abstracción
+// 6. Poliformismo
